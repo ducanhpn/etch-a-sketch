a76eb0b25df9b496951dd39e86d356b00a4349f4
Anh Pham <phamngocducanh19@gmail.com>
2022-09-14 16:31:54 +0200

    Change some details

--- script.js
+++ script.js
@@ -4,22 +4,23 @@ const container = document.getElementById("container");
 //get button
 const sizingButton = document.getElementById("btn");
 
-//create 16 divs
+//create 16 divs and hover effect
 function createDiv(numberPerSize){
     //width and height of square
     let sizeOfSquare = 1600 / numberPerSize;
-    console.log(sizeOfSquare);
     for(let i=0; i<(numberPerSize**2); i++){
         divArray.push(document.createElement("div"));
-        divArray[i].textContent = i+1;
         divArray[i].classList.add("square");
         divArray[i].style.cssText = `width: ${sizeOfSquare}px; height:${sizeOfSquare}px;`;
         divArray[i].addEventListener("mouseenter", () => divArray[i].classList.add("changeColor"));
         container.appendChild(divArray[i]);
     }
 }
+
+//init divs when page load
 createDiv(16);
 
+//clear all divs and array's elements
 function removeChild(){
     for(let i=0; i<(divArray.length); i++){
         container.removeChild(divArray[i]);
@@ -29,7 +30,7 @@ function removeChild(){
 
 
 
-//create action for button
+//create action for sizingButton
 function resizeDiv(){
     let divPerSize = prompt("Please enter a number of squares for each size (max. 100)");
     let inputNumber = parseInt(divPerSize);
